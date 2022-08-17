bdaa7b0b695ee07de591464a092c0c83d01374b2
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-17 20:14:06 +0300

    adds form validation

--- src/views/AuthPage/AuthPage.vue
+++ src/views/AuthPage/AuthPage.vue
@@ -3,12 +3,26 @@
     <h1 class="title bold">Sign In</h1>
     <div class="mb-3">
       <label for="email">Email:</label>
-      <input type="text" id="email" class="form-control" />
+      <input
+        type="text"
+        id="email"
+        :class="['form-control', { 'is-invalid': emailError }]"
+        v-model="email"
+      />
+      <span v-if="emailError" class="error-message">{{ emailError }}</span>
     </div>
 
     <div class="mb-3">
       <label for="password">Password:</label>
-      <input type="text" id="password" class="form-control" />
+      <input
+        type="text"
+        id="password"
+        :class="['form-control', { 'is-invalid': passwordError }]"
+        v-model="password"
+      />
+      <span v-if="passwordError" class="error-message">{{
+        passwordError
+      }}</span>
     </div>
 
     <button class="btn btn-success" type="submit">Sign In</button>
@@ -16,7 +30,31 @@
 </template>
 
 <script>
-export default {};
+import { useField } from "vee-validate";
+import * as yup from "yup";
+export default {
+  setup() {
+    const {
+      value: email,
+      errorMessage: emailError,
+      handleBlur: emailBlur,
+    } = useField("email", yup.string().trim().required().email());
+    const {
+      value: password,
+      errorMessage: passwordError,
+      handleBlur: passwordBlur,
+    } = useField("password", yup.string().trim().required().min(6));
+
+    return {
+      email,
+      emailError,
+      emailBlur,
+      password,
+      passwordError,
+      passwordBlur,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -29,5 +67,10 @@ label {
 .title {
   font-size: 35px;
   margin-bottom: 15px;
+}
+.error-message {
+  display: block;
+  margin-top: 5px;
+  color: var(--bs-danger);
 }
 </style>
