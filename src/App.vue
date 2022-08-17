3944dfaa2580ff4b175bf8ab01736b725cd557d0
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-17 16:14:48 +0300

    adds navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <div class="card">
+    <header class="card">
+      <TheNavbar />
+    </header>
+    <div class="card center">
       <nav>
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
@@ -9,6 +12,13 @@
     </div>
   </div>
 </template>
+
+<script>
+import TheNavbar from "./components/TheNavbar";
+export default {
+  components: { TheNavbar },
+};
+</script>
 
 <style>
 @import "~bootstrap/dist/css/bootstrap.css";
@@ -24,5 +34,10 @@
   border-radius: 10px;
   box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.2);
   background-color: rgb(231, 222, 222);
+}
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
---
+++ src/layout/MainLayout/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h2>Main Layout</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
