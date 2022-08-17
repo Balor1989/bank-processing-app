3254130e1ea10e1870054e4360b37f70d210fbfe
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-17 12:38:29 +0300

    adds bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="container">
+    <div class="card">
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style>
+@import "~bootstrap/dist/css/bootstrap.css";
+
+.container {
+  padding: 40px 0;
+}
+
+.card {
+  overflow: hidden;
+  padding: 15px;
+  margin-bottom: 15px;
+  border-radius: 10px;
+  box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.2);
+  background-color: rgb(231, 222, 222);
+}
+</style>
