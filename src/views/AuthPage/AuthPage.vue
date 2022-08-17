7b5df266dcf16ef7053b476eac8b5aea06d48103
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-17 21:19:37 +0300

    adds new  validation to AuthPage and fix bugs

--- src/views/AuthPage/AuthPage.vue
+++ src/views/AuthPage/AuthPage.vue
@@ -1,10 +1,10 @@
 <template>
-  <form class="card" @submit.prevent>
+  <form class="card" @submit.prevent="onSubmit">
     <h1 class="title bold">Sign In</h1>
     <div class="mb-3">
       <label for="email">Email:</label>
       <input
-        type="text"
+        type="email"
         id="email"
         :class="['form-control', { 'is-invalid': emailError }]"
         v-model="email"
@@ -15,7 +15,7 @@
     <div class="mb-3">
       <label for="password">Password:</label>
       <input
-        type="text"
+        type="password"
         id="password"
         :class="['form-control', { 'is-invalid': passwordError }]"
         v-model="password"
@@ -25,33 +25,53 @@
       }}</span>
     </div>
 
-    <button class="btn btn-success" type="submit">Sign In</button>
+    <button
+      class="btn btn-success"
+      type="submit"
+      :disabled="isSubmitting || isToManyAttempts"
+    >
+      Sign In
+    </button>
+    <span v-if="isToManyAttempts" class="error-message"
+      >To many attempts, please wait</span
+    >
   </form>
 </template>
 
 <script>
-import { useField } from "vee-validate";
+import { computed, watch } from "vue";
+import { useField, useForm } from "vee-validate";
 import * as yup from "yup";
 export default {
   setup() {
-    const {
-      value: email,
-      errorMessage: emailError,
-      handleBlur: emailBlur,
-    } = useField("email", yup.string().trim().required().email());
-    const {
-      value: password,
-      errorMessage: passwordError,
-      handleBlur: passwordBlur,
-    } = useField("password", yup.string().trim().required().min(6));
+    const { handleSubmit, isSubmitting, submitCount } = useForm();
+
+    const { value: email, errorMessage: emailError } = useField(
+      "email",
+      yup.string().trim().required().email()
+    );
+    const { value: password, errorMessage: passwordError } = useField(
+      "password",
+      yup.string().trim().required().min(6)
+    );
+
+    const isToManyAttempts = computed(() => submitCount.value >= 5);
+    watch(isToManyAttempts, (values) => {
+      if (values) {
+        setTimeout(() => (submitCount.value = 0), 3000);
+      }
+    });
+
+    const onSubmit = handleSubmit(() => {});
 
     return {
       email,
       emailError,
-      emailBlur,
       password,
       passwordError,
-      passwordBlur,
+      onSubmit,
+      isSubmitting,
+      isToManyAttempts,
     };
   },
 };
