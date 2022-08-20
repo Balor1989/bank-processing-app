acd65b501841e4356e2ff8b231b28311f24bf2ff
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-20 12:32:44 +0300

    fix modal

--- src/components/AppModal/AppModal.vue
+++ src/components/AppModal/AppModal.vue
@@ -9,7 +9,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="modalLabel">Modal title</h5>
+          <h5 class="modal-title" id="modalLabel">{{ title }}</h5>
           <button
             type="button"
             class="btn-close"
@@ -26,7 +26,7 @@
           >
             Close
           </button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-success">Save changes</button>
         </div>
       </div>
     </div>
