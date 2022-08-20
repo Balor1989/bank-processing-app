d9eb6fb704b3a100a6ee886515ad95c918bbada4
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-20 21:47:04 +0300

    fix 401 server message - redirect to auth

--- src/axios/request.js
+++ src/axios/request.js
@@ -1,7 +1,15 @@
+import router from "@/router";
 import axios from "axios";
 
 const requestAxios = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
+});
+
+requestAxios.interceptors.response.use(null, (error) => {
+  if (error.response.status === 401) {
+    router.push("/auth?message=needAuth");
+  }
+  return Promise.reject(error);
 });
 
 export default requestAxios;
