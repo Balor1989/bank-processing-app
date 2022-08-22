c596e3aebf1ea858b15a0467959d20f11af90a74
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-22 21:39:25 +0300

    adds operation request by id

--- src/views/RequestPage/RequestPage.vue
+++ src/views/RequestPage/RequestPage.vue
@@ -1,10 +1,36 @@
 <template>
-  <AppPage title="Operation" />
+  <AppLoader v-if="loading" />
+  <AppPage v-else-if="request" title="Operation" />
+  <div v-else class="card center">
+    <h3 class="medium">Operation was not found</h3>
+  </div>
 </template>
 
 <script>
 import AppPage from "@/components/AppPage";
-export default { components: { AppPage } };
+import { useRoute } from "vue-router";
+import { onMounted, ref } from "vue";
+import { useStore } from "vuex";
+import AppLoader from "@/components/AppLoader";
+export default {
+  components: { AppPage, AppLoader },
+
+  setup() {
+    const loading = ref(false);
+    const route = useRoute();
+    const store = useStore();
+    const request = ref({});
+
+    onMounted(async () => {
+      loading.value = true;
+      request.value = await store.dispatch("request/loadById", route.params.id);
+
+      loading.value = false;
+    });
+
+    return { loading, request };
+  },
+};
 </script>
 
 <style></style>
