8f38893811b9dce09a3466198151a59c04a88f56
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-22 22:42:46 +0300

    adds "go back" link

--- src/components/AppPage/AppPage.vue
+++ src/components/AppPage/AppPage.vue
@@ -1,4 +1,7 @@
 <template>
+  <div v-if="back">
+    <router-link class="back bold" to="/">Go back</router-link>
+  </div>
   <div class="card">
     <h2
       class="bold d-flex justify-content-between align-items-center main-title"
@@ -16,6 +19,10 @@ export default {
       type: String,
       required: true,
     },
+    back: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   setup(props) {
@@ -29,5 +36,19 @@ export default {
   margin-bottom: 25px;
   padding-bottom: 20px;
   border-bottom: 1px solid rgb(187, 186, 186);
+}
+.back {
+  display: inline-block;
+  margin-left: 10px;
+  margin-bottom: 10px;
+  color: rgb(150, 148, 148);
+  background-color: rgb(231, 222, 222);
+  padding: 8px 12px;
+  border-radius: 30px;
+  transition: color 300ms ease;
+}
+.back:hover,
+.back:focus {
+  color: #000;
 }
 </style>
--- src/views/RequestPage/RequestPage.vue
+++ src/views/RequestPage/RequestPage.vue
@@ -1,6 +1,11 @@
 <template>
   <AppLoader v-if="loading" />
-  <AppPage v-else-if="request" title="Operation" />
+  <AppPage back v-else-if="request" title="Operation">
+    <p><span>Name: </span>{{ request.name }}</p>
+    <p><span>Phone: </span>{{ request.phone }}</p>
+    <p><span>Amount: </span>{{ amountFormat(request.amount) }}</p>
+    <p><span>Status: </span><AppStatus :type="request.status" /></p>
+  </AppPage>
   <div v-else class="card center">
     <h3 class="medium">Operation was not found</h3>
   </div>
@@ -12,11 +17,13 @@ import { useRoute } from "vue-router";
 import { onMounted, ref } from "vue";
 import { useStore } from "vuex";
 import AppLoader from "@/components/AppLoader";
+import AppStatus from "@/components/AppStatus/AppStatus.vue";
+import { amountFormat } from "@/utils/amountFormat";
 export default {
-  components: { AppPage, AppLoader },
+  components: { AppPage, AppLoader, AppStatus },
 
   setup() {
-    const loading = ref(false);
+    const loading = ref(true);
     const route = useRoute();
     const store = useStore();
     const request = ref({});
@@ -28,7 +35,7 @@ export default {
       loading.value = false;
     });
 
-    return { loading, request };
+    return { loading, request, amountFormat };
   },
 };
 </script>
